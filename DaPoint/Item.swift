c12813a8769b89b5accf8c812668b01eeed3454a
philippe <philippe.lam.ny@gmail.com>
2026-07-07 11:24:04 +0200

    Initial Commit

---
+++ DaPoint/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DaPoint
+//
+//  Created by philippe on 07/07/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
