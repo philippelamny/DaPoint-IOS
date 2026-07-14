2efcf85a40bf404dc52547ba9a2861edca8ac5f0
philippe <philippe.lam.ny@gmail.com>
2026-07-14 13:25:22 +0200

    mise à jour

--- docs/appli/flutter_bootstrap.js
+++ docs/appli/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1359816550" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2182961431" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
